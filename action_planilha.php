cf0274c803c2012fdcb8ff43066d58ee917e5137
bruno <bruno.paula.carvalho@escola.pr.gov.br>
2025-12-02 13:52:19 -0300

    Update action_planilha.php

--- action_planilha.php
+++ action_planilha.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // URL do Apps Script
-    $url = "https://script.google.com/macros/s/SEU_ID_AQUI/exec";
+    $url = "https://script.google.com/macros/s/AKfycbzUryih7Y5ywVUldVg9MG2u5_bORs9Ec-J6Jz4FLXjxEQsR1BIPxS_9R4YZmpg3kyBmmg/exec";
 
     // Dados do formulário
     $data = [
@@ -21,9 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $response = curl_exec($ch);
     curl_close($ch);
 
-    echo "<p>Dados enviados para a planilha com sucesso!</p>";
+    echo "<p>enviaremos seu pato!</p>";
     echo "<pre>" . htmlspecialchars($response) . "</pre>";
     } else {
-        echo "<p>Nenhum dado foi enviado.</p>";
+        echo "<p>Nenhum pato vai ser enviado, nao recebos suas info.</p>";
     }
 ?>
